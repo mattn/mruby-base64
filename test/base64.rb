7676e97b60b049ebc71e4bdd91a3d5f4bea04c9c
mattn <mattn.jp@gmail.com>
2012-12-25 21:59:06 +0900

    add test.

---
+++ test/base64.rb
@@ -0,0 +1,8 @@
+
+assert('Base64 encode "ruby"') do
+  Base64::encode('ruby') == 'cnVieQ=='
+end
+
+assert('Base64 decode for old example') do
+  Base64::decode('44GK5YmN44Gv44Gp44GT44Gu44Ov44Kr44Oh44GY44KD') == "お前はどこのワカメじゃ"
+end
