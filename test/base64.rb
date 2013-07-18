47049841369385d93b208bf04e9ba6e74736d271
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-07-18 14:03:13 -0500

    Add out_len parameter to base64_encode/base64_decode
    This allows data with null characters to be decoded correctly.

--- test/base64.rb
+++ test/base64.rb
@@ -6,3 +6,7 @@ end
 assert('Base64 decode for old example') do
   Base64::decode('44GK5YmN44Gv44Gp44GT44Gu44Ov44Kr44Oh44GY44KD') == "お前はどこのワカメじゃ"
 end
+
+assert('Base64 decode null characters') do
+  Base64::decode('AGZvbw==') == "\000foo"
+end
